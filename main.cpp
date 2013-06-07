d003c8d906f05907c9daeb862ad473d6ba320a65
Cristiano Prato <cristiano.prato@zirak.it>
2013-06-07 16:11:36 +0200

    Remove hardcoded style sheet.
    Apply external style sheet by using qss file.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "dialog.h"
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFile qss(":/qss/qss/darkorange.qss");
+    qss.open(QFile::ReadOnly);
+    a.setStyleSheet(qss.readAll());
+    qss.close();
+
     Dialog w;
     w.show();
     
